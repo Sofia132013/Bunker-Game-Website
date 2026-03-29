e4ef5e9fa07de74e84ff048179b1a8efd46ba8b3
Sofi0407 <selyutinasofi@yandex.ru>
2026-03-29 17:54:55 +0300

    Fixed the code according to the coments

--- Bunker/server.js
+++ Bunker/server.js
@@ -1,48 +1,117 @@
-import cors from "cors";
-import express from "express";
-import helmet from "helmet";
-import compression from "compression";
-import dotenv from "dotenv";
-// import { PrismaClient } from '@prisma/client';
-import { PrismaClient } from "./generated/prisma/index.js";
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
+import dotenv from 'dotenv';
+import validPipe from './middlewares/validPipe.js';
+import prisma from '../lib/prisma.js';
+import Joi from 'joi';
+
+const PORT = process.env.PORT || 3000;
+const PREFIX = '/api/v1';
 
 dotenv.config();
-const prisma = new PrismaClient();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
-const PREFIX = "/api/v1";
+
+function generateUniqueCode() {
+    const chars = '0123456789';
+    let code = '';
+    const length = 6;
+    for (let i = 0; i < length; i++) {
+        code += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return code;
+}
 
 app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.get(`${PREFIX}/health`, (req, res) => {
-  res.json({
-    status: "ok",
-    timestamp: new Date().toISOString(),
-    uptime: process.uptime(),
-  });
-});
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    res.json({ status: 'ok',
+        timestamp: new Date().toISOString(),
+        uptime: process.uptime(),
+     });
 });
 
-function validPipe(schema) {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) {
-      return res
-        .status(400)
-        .json({ error: error.details.map((detail) => detail.message) });
+app.get(`${PREFIX}/users`, async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-    next();
-  };
+});
+
+app.get(`${PREFIX}/users/:id`, async (req, res) => {
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: parseInt(req.params.id) },
+        });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        console.error('Error fetching user:', err);
+        res.status(500).json({ error: 'Internal Server Error' });   
+    }
+
+}); 
+
+const userSchema = Joi.object({ name: Joi.string().required() })
+
+app.post(`${PREFIX}/users`, validPipe(userSchema), async (req, res) => {
+    try {
+        const { name } = req.body;
+        const user = await prisma.user.create({
+        data: {
+            name,
+            playerId: generateUniqueCode(),
+        },
+        });
+        res.status(201).json(user);
+    } catch (err) {
+        console.error('Error creating user:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+/*app.post(`${PREFIX}/data`, validPipe(schema), (req, res) => {
+    res.json({ message: 'Data is valid' });
+});*/
+function closeServer(server) {
+    return new Promise((resolve, reject) => {
+        server.close(err => {
+            if (err) {
+                reject(err); 
+            } else {
+                resolve();   
+            }
+        });
+    });
 }
 
-function gracefulShutdown() {
-  console.log("Shutting down gracefully...");
-  process.exit(0);
+async function gracefulShutdown() {
+    console.log('Shutting down gracefully...');
+    try{
+        await closeServer(server);
+        await prisma.$disconnect();
+        process.exit(0);
+
+    }catch (err) {
+        console.error('Shutdown error:', err);
+        process.exit(1);
+    }
 }
 
-process.on("SIGINT", gracefulShutdown);
-process.on("SIGTERM", gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+
