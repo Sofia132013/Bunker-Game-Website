35dfa7900a643d6f8e395d7b5561d70cf0acd2e5
Sofi0407 <selyutinasofi@yandex.ru>
2026-04-05 21:08:34 +0300

    chore(lobby.socket): create socket index file

---
+++ Bunker/socket/index.js
@@ -0,0 +1,25 @@
+import { Server } from 'socket.io';
+import lobbySocket from './lobby.socket.js';
+
+let socket;
+
+export function Socket(server){
+    socket = new Server(
+        server,
+        {
+            cors: {
+                origin: "*",
+                methods: ['GET', 'POST'],
+            },
+        }
+    );
+
+    lobbySocket(socket);
+    return socket;
+}
+
+
+export function getSocket() {
+    if (!socket) throw new Error('Socket.io not initialized!');
+    return socket;
+}
